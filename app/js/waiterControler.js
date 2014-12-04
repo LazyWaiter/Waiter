73147b70edd1152b129d4bcfe519ddd10078f828
christopheroz <roz.christophe@gmail.com>
2014-12-04 21:55:50 +0100

    supprimé:        app/index-async.html 	modifié:         app/index.html 	modifié:         app/js/waiterControler.js 	modifié:         app/waiterApp/waiterView.html

--- app/js/waiterControler.js
+++ app/js/waiterControler.js
@@ -1,11 +1,16 @@
 var waiterControllers = angular.module('lazyWaiter', []);
-waiterControllers.controller('ordersListCtrl', function ($scope) {
+waiterControllers.controller('ordersListCtrl', ['$scope','$http',function ($scope, $http) {
 
-    http.get('//lazywaiter.couchappy.com/orders/_design/orders/_view/all').
+    $http.get('//lazywaiter.couchappy.com/orders/_design/orders/_view/all').
             success(function (data, status, headers, config) {
-                $scope.orders = data.rows;
+               console.log(data.rows);   
+               console.log(status); 
+               console.log(headers); 
+               console.log(config); 
+                 $scope.orders = data.rows;
 console.log(data.rows);
             }).error(function (data, status, headers, config) {
 
     });
-});
+
+}]);
